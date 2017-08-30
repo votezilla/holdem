4310d3109146db745f05f8d65d0db707849b1d08
Aaron Smith <magicsquare15@gmail.com>
2017-08-30 14:29:33 +0700

    Sorting by rank, suit

--- holdem.go
+++ holdem.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sort"
 	"strconv"
 	"time"
 )
@@ -14,11 +15,13 @@ const (
 	
 	kNumSuits = 4
 	kNumCards = kNumSuits * kNumRanks
+	
+	kNumCardsPerHand = 5
 )
 
 type card struct {
-	iSuit	int
-	iRank	int
+	Suit	int
+	Rank	int
 }
 
 func assert(valid bool, errorMsg string) {
@@ -30,11 +33,11 @@ func assert(valid bool, errorMsg string) {
 func Card(perm int) (c card) {
 	assert(0 <= perm && perm < kNumCards, "perm is out of range")
 	
-	c.iSuit = perm % kNumSuits
-	c.iRank = perm / kNumSuits
+	c.Suit = perm % kNumSuits
+	c.Rank = perm / kNumSuits
 	
-	assert(0 <= c.iSuit && c.iSuit < kNumSuits, "suit is out of range")
-	assert(0 <= c.iRank && c.iRank < kNumRanks, "rank is out of range")
+	assert(0 <= c.Suit && c.Suit < kNumSuits, "suit is out of range")
+	assert(0 <= c.Rank && c.Rank < kNumRanks, "rank is out of range")
 
 	return c
 }
@@ -42,7 +45,7 @@ func Card(perm int) (c card) {
 func (c card) String() string {
 	var rank, suit string
 
-	r := c.iRank + kLowestRank
+	r := c.Rank + kLowestRank
 	switch r {
 		case kAceRank:		rank = "A";
 		case kAceRank - 1:	rank = "K";
@@ -51,7 +54,7 @@ func (c card) String() string {
 		default:			rank = strconv.Itoa(r);
 	}
 	
-	switch c.iSuit {
+	switch c.Suit {
 		case 0: suit = "c";
 		case 1: suit = "d";
 		case 2: suit = "h";
@@ -62,21 +65,42 @@ func (c card) String() string {
 	return rank + suit
 }
 
+type ByRank []card
+func (c ByRank) Len() int           { return len(c) }
+func (c ByRank) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
+func (c ByRank) Less(i, j int) bool { return c[i].Rank < c[j].Rank }
+
+type BySuit []card
+func (c BySuit) Len() int           { return len(c) }
+func (c BySuit) Swap(i, j int)      { c[i], c[j] = c[j], c[i] }
+func (c BySuit) Less(i, j int) bool { return c[i].Suit < c[j].Suit }
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	
 	for i := 0; i < 10; i++ {
 		deck := rand.Perm(kNumCards)
 
-		//fmt.Printf("deck = %v\n", deck)
+		fmt.Printf("New hand:\n")
 
-		hand := deck[0:5]
-
-		for _, h := range hand {
-			c := Card(h)
-			fmt.Printf("%v ", c)
+		hand := make([]card, kNumCardsPerHand)
+		for j := 0; j < kNumCardsPerHand; j++ {
+			hand[j] = Card(deck[j])
+			fmt.Printf("%v ", hand[j])
 		}
 		
-		fmt.Print("\n")
+		fmt.Printf("Sorted by rank:\n")
+		sort.Sort(ByRank(hand))
+		for _, h := range hand {
+			fmt.Printf("%v ", h)
+		}
+		
+		fmt.Printf("Sorted by suit:\n")
+		sort.Sort(BySuit(hand))
+		for _, h := range hand {
+			fmt.Printf("%v ", h)
+		}
+		
+		fmt.Print("\n\n")
 	}
 }
